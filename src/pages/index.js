254821d800e160d25c90148e8d0f3f73b04c8609
abdullahe <abdullahe@gmail.com>
2020-10-03 18:34:29 +0300

    index icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-<<<<<<< HEAD
 import { FaChevronDown } from "react-icons/fa/index"
 import { IconContext } from "react-icons"
-=======
->>>>>>> 063978d59f74103da45d5880a61ebd2e77798e3c
 
 const IndexPage = () => (
   <Layout>
@@ -16,12 +13,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-<<<<<<< HEAD
     <IconContext.Provider value={{ className: "icon" }}>
       <FaChevronDown />
     </IconContext.Provider>
-=======
->>>>>>> 063978d59f74103da45d5880a61ebd2e77798e3c
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
